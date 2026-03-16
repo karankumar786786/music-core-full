c669e07086c72ee5321e0e70ea5a46ae585114c6
karankumar786786 <onestream6969@gmail.com>
2026-03-17 00:52:41 +0530

    only base app

--- music-backend/src/auth/dto/auth.res.dto.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-import { createZodDto } from 'nestjs-zod';
-
-const AuthResSchema = z.object({
-    access_token: z.string(),
-});
-
-export class AuthResDto extends createZodDto(AuthResSchema) { }
--- reccomendationEngine/lib/helpers/inngest/__init__.py
+++
@@ -1,4 +0,0 @@
-from lib.helpers.inngest.client import inngest_client as client
-from lib.helpers.inngest.functions import functions
-
-__all__ = ["client", "functions"]
